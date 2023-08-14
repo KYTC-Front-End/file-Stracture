cd281a0905206de64b7625acae60f78cba8408d5
yasmeen attallah <yattallah7@gmail.com>
2023-08-14 14:47:18 +0300

    Update App.js

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { ThemeProvider } from '@mui/material/styles';
 import {Layout, Landing} from '../Pages';
+import Card from '../Components/Cards'
+import theme from './theme';
+
 
 function App() {
   return (
-    <Layout className="App">
+  <ThemeProvider theme = {theme}>
+         <Layout className="App">
+      <Card house={{location : "gaza", title : "roof" , description: "vhjn"}} />
     <Landing/>
     </Layout>
+
+  </ThemeProvider>
+ 
+
   );
 }
 
